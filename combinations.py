f00a612e23df5c0b975482dd29beef71e0401353
Lidiia Kliuchna <lida.kluchnaya@gmail.com>
2023-04-17 21:26:28 +0100

    commit

---
+++ combinations.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+N = int(input())
+S = input().split(' ')
+K = int(input())
+
+num = 0
+den = 0
+
+for c in combinations(S, K):
+
+    den += 1
+    num += 'a' in c
+
+print(round(num / den,4))
